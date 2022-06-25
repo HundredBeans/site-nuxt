5d9cb6f56dfe4f2c5011416fb856c8cf1a1f7839
Mohammad Daffa <m.daffa100@yahoo.com>
2022-06-25 17:59:02 +0700

    feat: add vueuse

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,8 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  buildModules: [
+  modules: [
     'nuxt-windicss',
+    '@vueuse/nuxt',
   ],
 })
