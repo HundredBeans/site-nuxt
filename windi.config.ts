b75ba46318431a12e0f6a62046973041c76af853
Mohammad Daffa <m.daffa100@yahoo.com>
2022-07-02 23:45:31 +0700

    feat: update windi config style

--- windi.config.ts
+++ windi.config.ts
@@ -3,11 +3,11 @@ import {defineConfig} from "windicss/helpers";
 export default defineConfig({
   shortcuts: {
     'bg-hard': 'bg-light-500 dark:bg-dark-800',
-    'bg-soft': 'bg-light-900 dark:bg-dark-400',
+    'bg-soft': 'bg-light-700 dark:bg-dark-400',
     'text-hard': 'text-dark-800 dark:text-light-900',
     'text-soft': 'text-dark-600 dark:text-neutral-300',
     'bg-button': 'bg-light-600 hover:bg-light-700 dark:bg-dark-700 hover:dark:bg-dark-600',
-    'border-hard': 'border-dark-800 dark:border-light-900',
+    'border-soft': 'border-1 border-slate-400 dark:border-gray-500',
   },
   plugins: [
     require('windicss/plugin/typography')({
