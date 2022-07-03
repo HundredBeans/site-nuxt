1423da5a43983ea060823417dca1cfd17e3b2154
Mohammad Daffa <m.daffa100@yahoo.com>
2022-07-03 20:56:22 +0700

    feat: add log on telegram webhook

--- server/api/telegram.post.ts
+++ server/api/telegram.post.ts
@@ -4,6 +4,9 @@ const telegramToken = runtimeConfig.telegram.token
 export default defineEventHandler(async (event) => {
   const body = await useBody(event)
   const query = useQuery(event)
+  console.log('telegram webhook called')
+  console.log('telegram webhook body', body)
+  console.log('telegram webhook query', query)
   if (query.token !== telegramToken) {
     return {
       message: "Token is not match"
