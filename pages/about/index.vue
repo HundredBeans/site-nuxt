0f9db06f2a1ed3e5f9bea3cbd6f91c7cc9265e5d
Mohammad Daffa <m.daffa100@yahoo.com>
2022-07-04 21:20:57 +0700

    feat: add projects and about pages

---
+++ pages/about/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="px-6">
+    <h1>About</h1>
+  </div>
+</template>
+
+<script setup>
+useHead({
+  title: "About",
+});
+</script>
---
+++ pages/projects/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="px-6">
+    <h1>Projects</h1>
+  </div>
+</template>
+
+<script setup>
+useHead({
+  title: "Projects",
+});
+</script>
