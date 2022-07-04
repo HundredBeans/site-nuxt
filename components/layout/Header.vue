3c8137788bd5b7d7984fe5843acfbd1633ed028e
Mohammad Daffa <m.daffa100@yahoo.com>
2022-07-04 07:08:53 +0700

    feat(style): add backdrop blur header

--- components/layout/Header.vue
+++ components/layout/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="flex gap-4 p-4 fixed blur-md w-full">
+  <nav class="flex gap-4 p-4 fixed backdrop-blur-md backdrop-filter w-full">
     <BaseSpacer />
     <template class="md:contents hidden">
       <div class="flex gap-6">
