c07aa0acc738ccb9601e1b23c05cb0528676070b
Mohammad Daffa <m.daffa100@yahoo.com>
2022-07-04 09:03:46 +0700

    feat: update link to fetch metadata

--- components/LinkCard.vue
+++ components/LinkCard.vue
@@ -1,11 +1,12 @@
 <template>
   <BaseCard>
     <span v-if="pending">Loading...</span>
+    <span v-else-if="!data">Data Not Found</span>
     <template v-else>
       <div class="flex flex-col text-left">
         <img
           class="!m-0 rounded-t-md h-36 w-full object-cover"
-          :src="data.image"
+          :src="data.images[0]"
         />
         <BaseLink :to="url" class="text-lg font-bold p-4 truncate no-underline">
           {{ data.title }}
@@ -22,7 +23,7 @@ const props = defineProps({
   },
 });
 const { pending, data } = useLazyFetch(
-  `https://oge.now.sh/api?url=${props.url}`
+  `https://jsonlink.io/api/extract?url=${props.url}`
 );
 watch(data, (newData) => {
   console.log("newData", newData);
