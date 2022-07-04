1b642b4feca53bf6daeb8467c93ae39e00a043f1
Mohammad Daffa <m.daffa100@yahoo.com>
2022-07-04 21:30:03 +0700

    feat: update meta-tags for icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,7 +16,10 @@ export default defineNuxtConfig({
   meta: {
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'manifest', href: '/site.webmanifest' }
+      { rel: 'apple-touch-icon', href: '/apple-touch-icon.png', sizes: '180x180' },
+      { rel: 'icon', type:'image/png', href: '/favicon-32x32.png', sizes: '32x32' },
+      { rel: 'icon', type:'image/png', href: '/favicon-16x16.png', sizes: '16x16' },
+      { rel: 'manifest', href: '/site.webmanifest' },
     ]
   },
   modules: [
