36cc510bc7f3c4fb762b01efb153d423e9602ea7
Mohammad Daffa <m.daffa100@yahoo.com>
2022-07-08 15:33:50 +0700

    debug: telegram reply markup

--- server/api/telegram.post.ts
+++ server/api/telegram.post.ts
@@ -9,16 +9,20 @@ const whitelistedUsername = ["Fadafuq"];
 const bookmark = new Bookmarks();
 
 /**
- * TODO: 
+ * TODO:
  * [ ] Add bookmark category (article, package, tools, etc.)
  * [ ] Add bookmark type (public or private)
  */
 const handleBookmarkBot = async (message) => {
   const { text, message_id: messageId } = message;
-  const {isValid, url} = parseUrl(text);
+  const { isValid, url } = parseUrl(text);
   if (isValid) {
     const title = text.replace(url, "").trim();
-    const returnedBookmark = await bookmark.addBookmark({ url, title, messageId });
+    const returnedBookmark = await bookmark.addBookmark({
+      url,
+      title,
+      messageId,
+    });
     return `Bookmark added with id: ${returnedBookmark.id}`;
   } else {
     return "Please input a valid url";
@@ -30,7 +34,7 @@ export default defineEventHandler(async (event) => {
   const { message } = body;
   const query = useQuery(event);
   const { token } = query;
-  console.log('body', body)
+  console.log("body", body);
 
   if (token !== telegramToken && token !== bookmarkToken) {
     return {
@@ -53,16 +57,18 @@ export default defineEventHandler(async (event) => {
       text: JSON.stringify(message, null, 2),
       reply_markup: {
         keyboard: [
-          {
-            text: "Testing 1"
-          },
-          {
-            text: "Testing 2"
-          }
+          [
+            {
+              text: "Testing 1",
+            },
+            {
+              text: "Testing 2",
+            },
+          ],
         ],
-        input_field_placeholder: "Select Category"
+        input_field_placeholder: "Select Category",
       },
-    }
+    };
     // return reply(bookmarkResult);
   }
   // TODO: Parse message
