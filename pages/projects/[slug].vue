d77eae10abbd63060bd977d1cc4cf4ec6bb218c3
Mohammad Daffa <m.daffa100@yahoo.com>
2022-07-17 19:21:37 +0700

    feat: add not-found slot

--- pages/projects/[slug].vue
+++ pages/projects/[slug].vue
@@ -8,31 +8,39 @@
         </span>
       </BaseLink>
       <br />
-      <ContentDoc v-slot="{ doc }" :path="`/projects/${$route.params.slug}`">
-        <h1>{{ doc.title }}</h1>
-        <div class="flex md:flex-row flex-col gap-4 md:gap-12">
-          <div>
-            <p class="!m-0 opacity-80">Type</p>
-            <p class="!m-0">{{ doc.type }}</p>
+      <ContentDoc :path="`/projects/${$route.params.slug}`">
+        <template #default="{ doc }">
+          <h1>{{ doc.title }}</h1>
+          <div class="flex md:flex-row flex-col gap-4 md:gap-12">
+            <div>
+              <p class="!m-0 opacity-80">Type</p>
+              <p class="!m-0">{{ doc.type }}</p>
+            </div>
+            <div>
+              <p class="!m-0 opacity-80">Period</p>
+              <p class="!m-0">{{ doc.period }}</p>
+            </div>
+            <div>
+              <p class="!m-0 opacity-80">Links</p>
+              <span>
+                <!-- eslint-disable-next-line vue/no-v-for-template-key -->
+                <template v-for="(link, idx) in doc.links" :key="link.url">
+                  <BaseLink class="!font-normal" :to="link.url" target="_blank">
+                    {{ link.text }}
+                  </BaseLink>
+                  {{ idx < doc.links.length - 1 ? " • " : "" }}
+                </template>
+              </span>
+            </div>
           </div>
-          <div>
-            <p class="!m-0 opacity-80">Period</p>
-            <p class="!m-0">{{ doc.period }}</p>
-          </div>
-          <div>
-            <p class="!m-0 opacity-80">Links</p>
-            <span>
-              <!-- eslint-disable-next-line vue/no-v-for-template-key -->
-              <template v-for="(link, idx) in doc.links" :key="link.url">
-                <BaseLink class="!font-normal" :to="link.url" target="_blank">
-                  {{ link.text }}
-                </BaseLink>
-                {{ idx < doc.links.length - 1 ? " • " : "" }}
-              </template>
-            </span>
-          </div>
-        </div>
-        <ContentRenderer :value="doc" />
+          <ContentRenderer :value="doc" />
+        </template>
+        <template #not-found>
+          <h1>Project not found</h1>
+          <p class="opacity-80">
+            The project you are looking for does not exist.
+          </p>
+        </template>
       </ContentDoc>
       <br />
       <BackToTop />
