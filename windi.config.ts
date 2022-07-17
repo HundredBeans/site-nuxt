15110d5b1ee572b18cc0b71e885ac37f10ff74fd
Mohammad Daffa <m.daffa100@yahoo.com>
2022-07-17 17:24:03 +0700

    feat: adjust ul styling windi

--- windi.config.ts
+++ windi.config.ts
@@ -1,21 +1,35 @@
-import {defineConfig} from "windicss/helpers";
+import { defineConfig } from "windicss/helpers";
 
 export default defineConfig({
   shortcuts: {
-    'bg-hard': 'bg-light-500 dark:bg-dark-800',
-    'bg-soft': 'bg-light-700 dark:bg-dark-400',
-    'text-hard': 'text-dark-800 dark:text-light-900',
-    'text-soft': 'text-dark-600 dark:text-neutral-300',
-    'bg-button': 'bg-light-600 hover:bg-light-700 dark:bg-dark-700 hover:dark:bg-dark-600',
-    'border-soft': 'border-1 border-slate-400 dark:border-gray-500',
+    "bg-hard": "bg-light-500 dark:bg-dark-800",
+    "bg-soft": "bg-light-700 dark:bg-dark-400",
+    "text-hard": "text-dark-800 dark:text-light-900",
+    "text-soft": "text-dark-600 dark:text-neutral-300",
+    "bg-button":
+      "bg-light-600 hover:bg-light-700 dark:bg-dark-700 hover:dark:bg-dark-600",
+    "border-soft": "border-1 border-slate-400 dark:border-gray-500",
   },
   plugins: [
-    require('windicss/plugin/typography')({
+    require("windicss/plugin/typography")({
       fonts: {
-        'body': 'Poppins, sans-serif',
+        body: "Poppins, sans-serif",
       },
-      dark: true
+      dark: true,
     }),
-    require('@windicss/plugin-icons'),
+    require("@windicss/plugin-icons"),
   ],
-})
+  theme: {
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            ul: {
+              padding: "0px",
+            },
+          },
+        },
+      },
+    },
+  },
+});
