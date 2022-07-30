ae70cf6986ee107082c33633c64320c674b08e42
Mohammad Daffa <m.daffa100@yahoo.com>
2022-07-30 21:36:25 +0700

    feat(style): adjust font-size for projectCard title

--- components/ProjectCard.vue
+++ components/ProjectCard.vue
@@ -1,7 +1,7 @@
 <template>
   <BaseCard class="p-6 text-left flex flex-col">
     <BaseLink :to="url">
-      <h2 class="!mt-0 !mb-4 !text-4xl">{{ title }}</h2>
+      <h2 class="!mt-0 !mb-4 !text-2xl">{{ title }}</h2>
     </BaseLink>
     <span class="opacity-80">{{ categories.join(" • ") }}</span>
     <p class="!mt-4 !mb-0">
