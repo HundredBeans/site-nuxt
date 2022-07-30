b92ca630b2e3fa63e4c35cd3fee82d8106516cf3
Mohammad Daffa <m.daffa100@yahoo.com>
2022-07-30 22:26:19 +0700

    feat: update scrollBehavior

--- app/router.options.ts
+++ app/router.options.ts
@@ -1,39 +1,22 @@
+// copied from https://github.com/nuxt/framework/issues/4888#issuecomment-1179582115
 import type { RouterConfig } from "@nuxt/schema";
 
 // https://router.vuejs.org/api/#routeroptions
 export default <RouterConfig>{
-  scrollBehavior: async (to, _from, savedPosition) => {
-    if (savedPosition) {
-      return savedPosition;
-    }
-
-    // https://dev.to/dimer191996/nuxt-js-smooth-scrolling-with-hash-links-94a
-    const findEl = (hash: string, x?: number) => {
-      return (
-        document.querySelector(hash) ||
-        new Promise((resolve, _reject) => {
-          if (x > 50) {
-            return resolve(findEl("body"));
-          }
-          setTimeout(() => {
-            resolve(findEl(hash, ++x || 1));
-          }, 100);
-        })
-      );
-    };
-
-    if (to.hash) {
-      const el = await findEl(to.hash);
-      if ("scrollBehavior" in document.documentElement.style) {
-        return window.scrollTo({ top: el.offsetTop, behavior: "smooth" });
-      } else {
-        return window.scrollTo(0, el.offsetTop);
-      }
-    }
-
+  scrollBehavior(to, _from, savedPosition) {
     return new Promise((resolve, _reject) => {
       setTimeout(() => {
-        resolve({ top: 0, behavior: "smooth" });
+        if (savedPosition) {
+          resolve(savedPosition);
+        } else if (to.hash) {
+          resolve({
+            el: to.hash,
+            top: 80,
+            behavior: "smooth",
+          });
+        } else {
+          resolve({ top: 0, behavior: "smooth" });
+        }
       }, 100);
     });
   },
