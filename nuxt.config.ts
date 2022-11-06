591004fb59e4e605edfe21a67889408222d99f20
Mohammad Daffa <m.daffa100@yahoo.com>
2022-11-06 15:59:06 +0700

    feat: update nuxt config structure

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,31 +21,33 @@ export default defineNuxtConfig({
       },
     },
   },
-  meta: {
-    htmlAttrs: {
-      lang: "en",
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: "en",
+      },
+      link: [
+        { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+        {
+          rel: "apple-touch-icon",
+          href: "/apple-touch-icon.png",
+          sizes: "180x180",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          href: "/favicon-32x32.png",
+          sizes: "32x32",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          href: "/favicon-16x16.png",
+          sizes: "16x16",
+        },
+        { rel: "manifest", href: "/site.webmanifest" },
+      ],
     },
-    link: [
-      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
-      {
-        rel: "apple-touch-icon",
-        href: "/apple-touch-icon.png",
-        sizes: "180x180",
-      },
-      {
-        rel: "icon",
-        type: "image/png",
-        href: "/favicon-32x32.png",
-        sizes: "32x32",
-      },
-      {
-        rel: "icon",
-        type: "image/png",
-        href: "/favicon-16x16.png",
-        sizes: "16x16",
-      },
-      { rel: "manifest", href: "/site.webmanifest" },
-    ],
   },
   modules: [
     "nuxt-windicss",
